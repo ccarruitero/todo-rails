01e461fa8eeeb4d7f2b8c2922b935db10cca98aa
Cesar Carruitero <cesar@mozilla.pe>
2012-08-23 00:34:49 -0500

    config generators

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,12 @@ module TodoRails
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+		config.generators do |g|
+			g.assets					false
+			g.helper					false
+			g.test_framework	false
+			g.stylesheets			false
+		end
   end
 end
